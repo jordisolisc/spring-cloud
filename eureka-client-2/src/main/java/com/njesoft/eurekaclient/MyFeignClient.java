33f917940d4f7f546d702b8f339e3310d686bfc5
SOLIS CALANCHA Jordi <jordi.solis-calancha@ext.ec.europa.eu>
2020-11-30 17:17:22 +0100

    BugFixing

--- eureka-client-2/src/main/java/com/njesoft/eurekaclient/MyFeignClient.java
+++ eureka-client-2/src/main/java/com/njesoft/eurekaclient/MyFeignClient.java
@@ -3,9 +3,9 @@ package com.njesoft.eurekaclient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(value = "${feign.name}", url = "${feign.url}", fallback = MyFeignClientFallbackImpl.class)
+@FeignClient(value="${feign.name}", url="${feign.url}", fallback = MyFeignClientFallbackImpl.class)
 public interface MyFeignClient {
-
-    @GetMapping
-    String client3Response();
+	
+	@GetMapping
+	String client3Response();
 }
--- eureka-client-2/src/main/java/com/njesoft/eurekaclient/MyFeignClientFallbackImpl.java
+++ eureka-client-2/src/main/java/com/njesoft/eurekaclient/MyFeignClientFallbackImpl.java
@@ -4,18 +4,19 @@ import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 import org.springframework.stereotype.Component;
 
-@Component()
-public class MyFeignClientFallbackImpl implements MyFeignClient{
-    @Override
-    public String client3Response() {
-        JSONObject jsonObject = new JSONObject();
-        try {
-            jsonObject.put("message", "Hello world 2");
-            jsonObject.put("message-3 ", "Hello world 8003 is down");
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+@Component
+public class MyFeignClientFallbackImpl implements MyFeignClient {
 
-        return jsonObject.toString();
-    }
+	@Override
+	public String client3Response() {
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("message", "Hello World 2");
+			jsonObject.put("message-3", "Hello World 8003 is down");
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return jsonObject.toString();
+	}
+
 }
